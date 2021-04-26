7a564b5f017d19bb53563baac49107f4b3c63483
Taylor Wright <taylorjohn.wright@gmail.com>
2021-04-26 14:11:13 -0400

    add comments, nice to have perks

--- init.el
+++ init.el
@@ -7,9 +7,15 @@
 
 (menu-bar-mode -1)          ; Disable the menu bar
 
+;; nicer font
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 140)
 
+;; title bar transparent to match theme
+(when (memq window-system '(mac ns))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)) ; nil for dark text
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)))
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -32,12 +38,25 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; add line numbers and column indicator
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; disable line numbers for some modes.
+(dolist (mode `(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode(lambda () (display-line-numbers-mode 0))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes counsel doom-modeline ivy use-package)))
+ '(package-selected-packages
+   '(which-key rainbow-delimiters rainbow-delimeters doom-themes counsel doom-modeline ivy use-package))
+ '(size-indication-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -45,6 +64,7 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; add ivy completion
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -64,12 +84,15 @@
   :config
   (ivy-mode 1))
 
+;; extended emacs commads for ivy
 (use-package counsel)
 
+;; a nicer looking theme
 (use-package doom-themes)
 
 (load-theme 'doom-nord t)
 
+;; more minimal modeline
 (use-package all-the-icons)
 
 (use-package doom-modeline
@@ -77,3 +100,13 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+;; rainbow delimiters to more easily see parenths
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; which-key gives available bindings for a prefix
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
